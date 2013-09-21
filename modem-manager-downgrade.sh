a0f1bee7d167f35111b52ec7f71d3d5f2465299a
Ghaith Belgacem <meteorhead@gmx.com>
2013-09-21 13:59:04 +0100

    fixed ebuild URL

--- modem-manager-downgrade.sh
+++ modem-manager-downgrade.sh
@@ -6,7 +6,8 @@ echo "area51" >> /usr/local/portage/profiles/repo_name
 PORT_OVERLAY="/usr/local/portage" 
 mkdir -p /usr/local/portage/net-misc/modemmanager/
 cp -pr /usr/portage/net-misc/modemmanager/files /usr/local/portage/net-misc/modemmanager
-cd /usr/local/portage/net-misc/modemmanager/ 
+cd /usr/local/portage/net-misc/modemmanager/
+wget http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/net-misc/modemmanager/modemmanager-0.5.2.0-r3.ebuild?revision=1.8 -O modemmanager-0.5.2.0-r3.ebuild
 ebuild modemmanager-0.5.2.0-r3.ebuild manifest
 eix-update
 echo  "=net-misc/modemmanager-0.6.0.0" >> /etc/portage/package.mask
